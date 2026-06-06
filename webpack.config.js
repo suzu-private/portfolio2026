67f6bccbf86357d8816722d34c656a0d3c8bc8d3
DESKTOP-O7K86RP\suzu_ <suzu_official@outlook.jp>
2026-06-06 17:49:45 +0900

    setup

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const htmlPlugins = glob
+  .sync('./src/html/*.pug')
+  .map(
+    (file) =>
+      new HtmlWebpackPlugin({
+        template: file,
+        filename: path.basename(file, '.pug') + '.html',
+      })
+  );
+
+const entries = Object.fromEntries(
+  glob.sync('./src/js/*.js').map((file) => [
+    path.basename(file, '.js'),
+    './' + file.replace(/\\/g, '/'),
+  ])
+);
+
+module.exports = {
+  entry: entries,
+
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    ...htmlPlugins,
+
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
+
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+    watchFiles: ['src/**/*'],
+  },
+};
